8e8c685031dae5c4628bdba7359c43919f8132c2
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-11-22 16:41:59 +0300

    create basic structure for Missions

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,13 +5,14 @@ export default function Header() {
   return (
     <div className="flex justify-between mt-5 shadow-lg pb-5">
       <img className="ml-20 w-[70px] h-[70px]" src="/planet.png" alt="img" />
-      <h1 className="ml-5  mt-2 text-4xl font-bold w-full align-middle">Space Travelers Hub</h1>
+      <h1 className="ml-5  mt-2 text-4xl font-bold w-full align-middle">Space Travelers&apos; Hub</h1>
       <div className="w-full flex justify-end">
         <ul className="flex justify-end w-full gap-5 mt-5 mr-10">
-          <NavLink className="text-blue-400 font-bold" to="/">Rockets</NavLink>
-          <NavLink className="text-blue-400 font-bold" to="mission">Mission</NavLink>
-          <NavLink className="text-blue-400 font-bold" to="dragon">Dragon</NavLink>
-          <NavLink className="border-l-2 px-5 h-[70%] border-indigo-500 text-blue-400 font-bold" to="/profile">profile</NavLink>
+          <NavLink className="text-primary font-bold" to="/">Rockets</NavLink>
+          <NavLink className="text-primary font-bold" to="mission">Missions</NavLink>
+          <NavLink className="text-primary font-bold" to="dragon">Dragons</NavLink>
+          <hr className="bg-secondary h-6 w-px" />
+          <NavLink className="text-primary font-bold" to="/profile">My Profile</NavLink>
         </ul>
       </div>
     </div>
--- src/pages/Mission.jsx
+++ src/pages/Mission.jsx
@@ -2,11 +2,86 @@ import React from 'react';
 import Button from '../UI/button';
 
 export default function Mission() {
-  const name = 'bg-pink-500';
+  const joinStyle = 'bg-transparent border-2 rounded p-2 text-secondary font-semibold text-sm';
+  const leaveStyle = 'bg-transparent border-2 border-alert rounded px-2 py-0 text-alert font-semibold text-sm'
+  const joinTitle = 'Join Mission';
+  const leaveTitle = 'Leave Mission';
   return (
-    <div>
-      <h1>Mission</h1>
-      <Button title="button click" id="id" className={name} />
-    </div>
+    <ul className="flex-col mt-4 mx-8">
+      <div className="kanaderalla">Hala</div>
+      <li className="flex justify-between font-bold even:bg-gray-100">
+        <h3 className="w-1/1 p-2 border">Mission</h3>
+        <h3 className="w-5/7 p-2 border">Description</h3>
+        <h3 className="w-1/1 p-2 border">Status</h3>
+        <div className="w-1/1 border" />
+      </li>
+      <li className="flex even:bg-gray-100">
+        <div className="w-1/1 p-2 border font-bold">Thaicom</div>
+        <div className="w-5/7 p-2 border text-sm font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          consequatur eum porro neque, cumque quidem nisi molestias dolor dolore
+          assumenda laborum veniam unde ipsa fuga illum aut quo quod maiores.
+        </div>
+        <div className="
+          flex
+          justify-center
+          items-center
+          w-1/1
+          p-2 border"
+        >
+          <div className="
+            bg-secondary
+            px-1
+            text-font
+            uppercase
+            place-content-center
+            rounded
+            font-semibold"
+          >
+            not a memeber
+          </div>
+        </div>
+        <div className="flex justify-center w-1/1 p-2 border">
+          <Button
+            title={joinTitle}
+            id="id"
+            className={joinStyle}
+          />
+        </div>
+      </li>
+      <li className="flex even:bg-gray-100">
+        <div className="w-1/1 p-2 border font-bold">Thaicom</div>
+        <div className="w-5/7 p-2 border text-sm font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          consequatur eum porro neque, cumque quidem nisi molestias dolor dolore
+          assumenda laborum veniam unde ipsa fuga illum aut quo quod maiores.
+        </div>
+        <div className="
+          flex
+          justify-center
+          items-center
+          w-1/1
+          p-2 border"
+        >
+          <div className="
+            bg-primary
+            px-1
+            text-font
+            place-content-center
+            rounded
+            font-semibold"
+          >
+            Active Member
+          </div>
+        </div>
+        <div className="flex justify-center w-1/1 p-2 border">
+          <Button
+            title={leaveTitle}
+            id="id"
+            className={leaveStyle}
+          />
+        </div>
+      </li>
+    </ul>
   );
 }
