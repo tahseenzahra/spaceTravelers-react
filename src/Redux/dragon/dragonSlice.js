d99997ab9343ce883c59f6b8d0484e85683ceca2
Ashe546 <asfyd42@gmail.com>
2022-11-23 06:03:48 -0300

    minnor error

--- src/Redux/dragon/dragonSlice.js
+++ src/Redux/dragon/dragonSlice.js
@@ -10,7 +10,7 @@ export const fetchDragons = createAsyncThunk(
     } catch (error) {
       return error.message;
     }
-  },
+  }
 );
 
 const dragonSlice = createSlice({
@@ -18,9 +18,10 @@ const dragonSlice = createSlice({
   initialState: { dragon: [], status: 'idle' },
   reducers: {
     dragonStatus: (state, { payload }) => {
-      const data = state.dragon.map((x) => (x.id === payload
-        ? { ...x, reservation: !x.reservation }
-        : x));
+      console.log(payload);
+      const data = state.dragon.map((x) =>
+        x.id === payload ? { ...x, reservation: !x.reservation } : x
+      );
       return {
         ...state,
         dragon: data,
@@ -34,7 +35,6 @@ const dragonSlice = createSlice({
     }))
       .addCase(fetchDragons.fulfilled, (state, action) => {
         const data = [];
-        /*eslint-disable */
         action.payload.map((x) => {
           const { id, flickr_images, description, name } = x;
           data.push({
@@ -46,7 +46,6 @@ const dragonSlice = createSlice({
           });
           return data;
         });
-        /*eslint-disable */
         return {
           ...state,
           dragon: data,
