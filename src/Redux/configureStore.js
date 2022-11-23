6ecda8e41f23cd3f5607a48ff64613dae2605afc
tahseenzahra <tahseen1zahra@gmail.com>
2022-11-24 04:48:48 +0500

    Create directory for redux state slice

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import dragonReducer from './dragon/dragonSlice';
+import rocketReducer from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
     dragons: dragonReducer,
+    rockets: rocketReducer,
   },
 });
 
