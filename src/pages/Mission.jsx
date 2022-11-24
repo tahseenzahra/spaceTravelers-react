12c0bf84fdb7e0c7f874e85cab6e21f8679d3934
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-11-24 15:56:20 +0300

    attempt localstorage

--- src/pages/Mission.jsx
+++ src/pages/Mission.jsx
@@ -1,40 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { loaded, missionStatus } from "../redux/missions/missionSlice";
 import Missions from "../UI/missions";
-import { loaded } from "../redux/missions/missionSlice";
 
 export default function Mission() {
-  const [isJoined, setJoined] = useState("false");
-  const dispatch = useDispatch();
   const { mission } = useSelector((state) => state.missions);
+  
+  const dispatch = useDispatch();
+  console.log('useeffect');
   useEffect(() => {
     dispatch(loaded());
   }, []);
-  const handleJoin = (e) => {
-    mission[0].join_mission = true;
+  const handleJoin = (id) => {
+    dispatch(missionStatus(id));
   };
   return (
     <ul
-      className="mission-container"
-      id="missionList"
+      className='mission-container'
+      id='missionList'
     >
-      <li className="mission-header">
-        <h3 className="mission-column">Mission</h3>
-        <h3 className="description-column">Description</h3>
-        <h3 className="status-column">Status</h3>
-        <div className="join-column" />
+      <li className='mission-header'>
+        <h3 className='mission-column'>Mission</h3>
+        <h3 className='description-column'>Description</h3>
+        <h3 className='status-column'>Status</h3>
+        <div className='join-column' />
       </li>
       {mission.map((mission) => (
         <Missions
           key={Math.random()}
           mission={mission.mission_name}
           description={mission.description}
-          status={isJoined ? "not a member" : "Active Member"}
-          statusStyle={isJoined ? "member-status inactive-member" : "member-status active-member"}
-          buttonTag={isJoined ? "Join Mission" : "Leave Mission"}
+          status={mission.join_mission ? "Active Member" : "not a member"}
+          statusStyle={
+            mission.join_mission
+              ? "member-status active-member"
+              : "member-status inactive-member"
+          }
+          buttonTag={mission.join_mission ? "Leave Mission" : "Join Mission"}
           id={mission.mission_id}
-          className={isJoined ? "mission-btn join-btn" : "mission-btn leave-btn"}
-          event={(e) => {handleJoin(e)}}
+          className={
+            mission.join_mission
+              ? "mission-btn leave-btn"
+              : "mission-btn join-btn"
+          }
+          event={() => handleJoin(mission.mission_id)}
         />
       ))}
     </ul>
--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -2,41 +2,60 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const APIURL = 'https://api.spacexdata.com/v3/missions';
-const ACCEPT_MISSION = 'ACCEPT_MISSION';
-const JOINED = 'JOINED';
 
-export const loaded = createAsyncThunk(ACCEPT_MISSION, async () => {
-  const response = await fetch(APIURL);
-  const data = await response.json();
-  return data;
-});
+export const loaded = createAsyncThunk(
+  'missions/loaded',
+  async () => {
+    try {
+      const response = await axios.get(APIURL);
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  });
 
-//export const joined = createAsyncThunk(JOINED, async ())
-
+  //if (localStorage.mission === undefined)
 const missionSlice = createSlice({
-  name: 'spaceTravelers',
-  initialState: { mission: [] },
-  reducers: {},
+  name: 'missions',
+  initialState: { mission: [], status: 'idle' },
+  reducers: {
+    missionStatus: (state, { payload }) => {
+      const data = state.mission.map((x) => (x.mission_id === payload ? { ...x, isJoined: !x.isJoined } : x));
+      localStorage.setItem('mission', JSON.stringify({data}))
+      return {
+        ...state,
+        mission: data,
+      };
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(
-      loaded.fulfilled, (state, action) => {
-        const datas = [];
-        action.payload.map((data) => {
-          const {
+    builder.addCase(loaded.pending, (state) => ({
+      ...state,
+      status: 'pending',
+    }))
+      .addCase(loaded.fulfilled, (state, action) => {
+        let missionData = [];
+        action.payload.map((mission) => {
+          const { description, mission_id, mission_name } = mission;
+          missionData.push({
+            description,
             mission_id,
             mission_name,
-            description,
-          } = data;
-          datas.push({ mission_id, mission_name, description, join_mission: false })
-          return datas;
-        })
-        return {
-          ...state,
-          mission: datas,
-        }
+            isJoined: false,
+          });
+          return missionData;
+        },
+        //console.log(JSON.stringify(missionData)),
+        //localStorage.setItem('mission', JSON.stringify({missionData}))
+        )
       }
-    );
+      )
+      .addCase(loaded.rejected, (state, { error }) => ({
+        ...state,
+        status: error,
+      }));
   },
 });
 
+export const { missionStatus } = missionSlice.actions;
 export default missionSlice.reducer;
