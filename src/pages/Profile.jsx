36513de40ec1bdd6bb0d37c757425c24007b567d
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-11-25 16:34:07 +0300

    add join missions to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
   const rocketsData = useSelector((state) => state.rockets);
-  let LocalDragon = []
-  
+  let LocalDragon = [];
+  let LocalMission = [];
+
   try {
     if (window !== "undefined")
       LocalDragon = JSON.parse(localStorage.getItem("dragon"));
-  } catch (e) { }
+      LocalMission = JSON.parse(localStorage.getItem("mission"));
+  } catch (e) {}
 
-
-  const rocketProfile = rocketsData.filter(rocket => rocket.reserve === true)
-  const dragonProfile = LocalDragon.filter(dra => dra.reservation === true) || []
+  const rocketProfile = rocketsData.filter((rocket) => rocket.reserve === true);
+  const dragonProfile =
+    LocalDragon.filter((dra) => dra.reservation === true) || [];
+  const missionProfile = LocalMission.filter((mission) => mission.isJoined === true) || [];
   return (
     <div className='flex justify-between px-40'>
-
       <div>
-        <table className="table-fixed mt-5 ml-5">
+        <table className='table-fixed mt-5 ml-5'>
           <thead>
             <tr>
-              <th className="text-2xl">Rockets</th>
+              <th className='text-2xl'>Rockets</th>
             </tr>
           </thead>
           <tbody>
             {rocketProfile.map((rocket) => (
               <tr>
-                <td className="border-solid border-2 border-grey-300 p-3 w-[500px]">{rocket.name}</td>
+                <td className='border-solid border-2 border-grey-300 p-3 w-[500px]'>
+                  {rocket.name}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -35,44 +38,42 @@ export default function Profile() {
       </div>
 
       <div>
-        <table className="table-fixed mt-5 ml-5">
+        <table className='table-fixed mt-5 ml-5'>
           <thead>
             <tr>
-              <th className="text-2xl">Mission</th>
+              <th className='text-2xl'>Mission</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="border-solid border-2 border-grey-300 p-3 w-[500px]">The Sliding</td>
+              {missionProfile.map((mission, i) => (
+            <tr key={i}>
+              <td className='border-solid border-2 border-grey-300 p-3 w-[500px]'>
+                {mission.name}
+              </td>
             </tr>
-            <tr>
-              <td className="border-solid border-2 border-grey-300 p-3 w-[500px]">The Sliding</td>
-            </tr>
-            <tr>
-              <td className="border-solid border-2 border-grey-300 p-3 w-[500px]">The Sliding</td>
-            </tr>
+              ))}
           </tbody>
         </table>
       </div>
 
-
       <div>
-        <table className="table-fixed mt-5 ml-5">
+        <table className='table-fixed mt-5 ml-5'>
           <thead>
             <tr>
-              <th className="text-2xl">Dragon</th>
+              <th className='text-2xl'>Dragon</th>
             </tr>
           </thead>
           <tbody>
             {dragonProfile.map((dra, i) => (
               <tr key={i}>
-                <td className="border-solid border-2 border-grey-300 p-3 w-[500px]">{dra.name}</td>
+                <td className='border-solid border-2 border-grey-300 p-3 w-[500px]'>
+                  {dra.name}
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-
     </div>
   );
 }
